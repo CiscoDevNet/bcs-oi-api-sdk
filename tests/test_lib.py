08ab6c6f652ecd20f3dac241ca7363c73d14b434
ashiprad <ashiprad@cisco.com>
2022-07-08 03:00:40 +0530

    added masking for apis

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,6 +1,7 @@
 import os
 from src.bcs_oi_api.bcs_oi_api import BCSOIAPI
 from src.bcs_oi_api.models import Device, DeviceFilter
+import pytest
 
 
 def test_lib_base_inventory_devices() -> None:
@@ -30,6 +31,52 @@ def test_lib_filter_inventory_devices() -> None:
     filter_ = DeviceFilter(device_id=[24932787, 24932788], inventory_status=["NotAvailable"])
 
     devices = bcs_oi_api.get_output(model=Device, filter_=filter_)
+    devices_dict = {device.device_id: device for device in devices}
+
+    assert len(devices_dict) == 2
+
+
+def test_lib_fields_inventory_devices() -> None:
+    client_id = os.getenv("SDK_TEST_CLIENT_ID")
+    client_secret = os.getenv("SDK_TEST_CLIENT_SECRET")
+
+    assert client_id is not None, "Test client id not set in environment"
+    assert client_secret is not None, "Test client secret not set in environment"
+
+    bcs_oi_api = BCSOIAPI(client_id=client_id, client_secret=client_secret, region="us")
+
+    devices = bcs_oi_api.get_output(model=Device, fields="deviceId")
+    devices_dict = {device.device_id: device for device in devices}
+
+    assert devices_dict != {}
+
+
+def test_lib_wrong_fields_inventory_devices() -> None:
+    client_id = os.getenv("SDK_TEST_CLIENT_ID")
+    client_secret = os.getenv("SDK_TEST_CLIENT_SECRET")
+
+    assert client_id is not None, "Test client id not set in environment"
+    assert client_secret is not None, "Test client secret not set in environment"
+
+    bcs_oi_api = BCSOIAPI(client_id=client_id, client_secret=client_secret, region="us")
+
+    devices = bcs_oi_api.get_output(model=Device, fields="deviceId")
+    with pytest.raises(AttributeError):
+        _ = {device.collector_name: device for device in devices}
+
+
+def test_lib_filter_fields_inventory_devices() -> None:
+    client_id = os.getenv("SDK_TEST_CLIENT_ID")
+    client_secret = os.getenv("SDK_TEST_CLIENT_SECRET")
+
+    assert client_id is not None, "Test client id not set in environment"
+    assert client_secret is not None, "Test client secret not set in environment"
+
+    bcs_oi_api = BCSOIAPI(client_id=client_id, client_secret=client_secret, region="us")
+
+    filter_ = DeviceFilter(device_id=[24932787, 24932788], inventory_status=["NotAvailable"])
+
+    devices = bcs_oi_api.get_output(model=Device, filter_=filter_, fields="deviceId")
     devices_dict = {device.device_id: device for device in devices}
 
     assert len(devices_dict) == 2
