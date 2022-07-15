7bed63c429c07e6e6756257ee8496b790636c518
ashiprad <ashiprad@cisco.com>
2022-07-15 14:10:17 +0530

    fixed uir summary response

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -3,7 +3,7 @@ import os
 import pytest
 
 from src.bcs_oi_api.bcs_oi_api import BCSOIAPI
-from src.bcs_oi_api.models import Device, DeviceFilter
+from src.bcs_oi_api.models import Device, DeviceFilter, UIRSummary
 
 
 def test_lib_base_inventory_devices() -> None:
@@ -82,3 +82,18 @@ def test_lib_filter_fields_inventory_devices() -> None:
     devices_dict = {device.device_id: device for device in devices}
 
     assert len(devices_dict) == 2
+
+
+def test_uir_summary() -> None:
+    client_id = os.getenv("SDK_TEST_CLIENT_ID")
+    client_secret = os.getenv("SDK_TEST_CLIENT_SECRET")
+
+    assert client_id is not None, "Test client id not set in environment"
+    assert client_secret is not None, "Test client secret not set in environment"
+
+    bcs_oi_api = BCSOIAPI(client_id=client_id, client_secret=client_secret, region="us")
+
+    uir_summary = bcs_oi_api.get_output(model=UIRSummary)
+    uir_summary_dict = next(uir_summary).dict()
+
+    assert uir_summary_dict != {}
