7321ca86ebc65508c0ce5f5123ed005fe8e5cf3f
pmoharan <pmoharan@cisco.com>
2024-02-06 15:49:00 +0530

    test case fix

--- src/bcs_oi_api/models/product_alerts/models.py
+++ src/bcs_oi_api/models/product_alerts/models.py
@@ -94,7 +94,6 @@ class HardwareEndOfLife(BCSOIAPIBaseModel):
     physical_asset_id: int
     physical_asset_type: str
     product_id: str
-    replacement_product_ids: Optional[list]
 
     @classmethod
     def url_path(cls) -> str:
@@ -116,7 +115,7 @@ class HardwareEndOfLifeBulletin(BCSOIAPIBaseModel):
     last_day_of_support_date: Optional[date]
     last_ship_date: Optional[date]
     product_id: str
-    replacement_product_ids: Optional[list]
+    replacement_product_id: Optional[list]
 
     @classmethod
     def url_path(cls) -> str:
