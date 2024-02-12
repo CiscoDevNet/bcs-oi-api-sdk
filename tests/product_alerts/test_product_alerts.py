d5bbee096782a5b883898157ad7fc73da3e99c93
pmoharan <pmoharan@cisco.com>
2024-02-12 17:21:19 +0530

    check fix

--- tests/product_alerts/test_product_alerts.py
+++ tests/product_alerts/test_product_alerts.py
@@ -109,7 +109,7 @@ hardware_end_of_life_bulletin_1: dict = {
     "lastDayOfSupportDate": "2024-05-31",
     "lastShipDate": "2019-08-04",
     "productId": "N5K-C5548UP",
-    "replacementProductId": "N5K-C5548UB",
+    "replacementProductIds": "N5K-C5548UB",
 }
 
 hardware_end_of_life_bulletin_2: dict = {
